f687ff3e0fdb06aadf3012e80400b9695a4a4956
nigel feng <nigelfeng@aaxischina.com>
2023-11-07 13:12:29 +0800

    test

--- App.tsx
+++ App.tsx
@@ -88,7 +88,6 @@ function App(): JSX.Element {
           </Section>
           <Section title="Learn More">
             Read the docs to discover what to do next:
-            test
           </Section>
           <LearnMoreLinks />
         </View>
